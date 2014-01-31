bf3ea119d9abdf9b5391bc3d34f8e0c419e384b4
Jonathan Cichon <jcic@pixelflut.net>
2014-01-31 16:21:35 +0100

    Alert and Actionsheet subclasses

---
+++ PxUIKit/PxUIKit/PxAlertView.m
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2013 pixelflut GmbH, http://pixelflut.net
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ */
+
+//
+//  PxAlertView.m
+//  PxUIKit
+//
+//  Created by Jonathan Cichon on 31.01.14.
+//  Copyright (c) 2014 pixelflut GmbH. All rights reserved.
+//
+
+#import "PxAlertView.h"
+
+@interface PxAlertView ()
+@property(nonatomic, strong, readonly) PxActionButtonConfig *config;
+
+@end
+
+@implementation PxAlertView
+
+- (id)initWithTitle:(NSString *)title message:(NSString *)message block:(void (^)(PxActionButtonConfig *buttonConfig))block {
+    self = [super init];
+    if (self) {
+        [self setTitle:title];
+        [self setMessage:message];
+        
+        _config = [[PxActionButtonConfig alloc] init];
+        block(_config);
+        [self.config reorderButtons];
+        
+        NSInteger c = [self.config cancelIndex];
+        
+        for (int i = 0; i<self.config.visibleButtonCount; i++) {
+            NSString *title = [self.config titleAtIndex:i];
+            [self addButtonWithTitle:title];
+            i == c ? [self setCancelButtonIndex:i] : nil;
+        }
+        
+        [self setDelegate:self];
+    }
+    return self;
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+	if(self.config) {
+        [self.config executeButtonAtIndex:buttonIndex];
+    }
+}
+
+@end
---
+++ PxUIKit/PxUIKit/PxInputAnimation.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2013 pixelflut GmbH, http://pixelflut.net
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ */
+
+//
+//  PxInputAnimation.h
+//  PxUIKit
+//
+//  Created by Jonathan Cichon on 31.01.14.
+//  Copyright (c) 2014 pixelflut GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ Wrapps the informations from keyboard (or picker) show/hide notifications to get access in an object-oriented way.
+ */
+@interface PxInputAnimation : NSObject
+@property (nonatomic, assign) CGRect startFrame;
+@property (nonatomic, assign) CGRect endFrame;
+@property (nonatomic, assign) CGFloat duration;
+@property (nonatomic, assign) UIViewAnimationCurve curve;
+@property (nonatomic, strong) NSString *name;
+
+- (id)initWithStartFrame:(CGRect)startFrame endFrame:(CGRect)endFrame duration:(CGFloat)duration curve:(UIViewAnimationCurve)curve name:(NSString *)name;
+//- (id)initWithPickerNotification:(NSNotification *)notification;
+- (id)initWithKeyboardNotification:(NSNotification *)notification;
+
+- (BOOL)willShow;
+
+@end
